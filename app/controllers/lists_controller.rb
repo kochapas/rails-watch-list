1830a9bc42e95b9e6ae61205cec9471b42e8a9ca
kochapas <kochapas@outlook.com>
2021-08-12 17:13:51 +0900

    Implement: Add/remove lists, add/remove bookmark. Fix styles.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,4 +5,25 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  def new
+    @list = List.new
+  end
+  def create
+    @list = List.new(strong_list)
+    if @list.save
+      redirect_to :lists
+    else
+      render :new
+    end
+  end
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to :lists
+  end
+
+  private
+  def strong_list
+    return params.require(:list).permit(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
 puts "Destroy Everything 🔫👽"
 Movie.destroy_all
 
